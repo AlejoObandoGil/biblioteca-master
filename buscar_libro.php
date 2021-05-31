b85112282a93c5232a5b671c57ac15298710022e
alejo <alejo.obando.g@gmail.com>
2021-05-31 14:09:32 -0500

    create ready

--- buscar_libro.php
+++ buscar_libro.php
@@ -1,31 +1,16 @@
 <?php 
 
 	include('conexion.php');
-    
-	$objeto = new Conexion();
-	$conexion = $objeto->Conectar();
 
     $titulo = $_POST['titulo'];
 
     $consulta = "SELECT * FROM libros WHERE titulo = '$titulo'";
 
-    $resultados = pg_query($conexion,$consulta);
+    $resultados = pg_query($conn,$consulta);
 
     while ($resultado = pg_fetch_array($resultados)){
 
         echo $resultado['codigo_libro']." ".$resultado['titulo']." ".$resultado['autor']." ".$resultado['fecha_publicacion']." ".$resultado['genero_literario']." ".$resultado['numero_paginas']." ".$resultado['editorial']." ".$resultado['issn']." ".$resultado['idioma']." ".$resultado['estado']." ".$resultado['precio']." ".$resultado['imagen_portada'];
     }
 
-
-	// $consulta = "SELECT * FROM libros";
- //    $resultado = $conexion->prepare($consulta);
- //    $resultado->execute();
- //    $datos = $resultado->fetchAll();
-
- //    var dump($datos); 
-
- //    foreach ($datos as dato) {
- //    	echo $dato['titulo']. "<br>";
- //    }
-
- ?>
+?>
