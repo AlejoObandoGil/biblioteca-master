5f9d4a02959d0f698e71a8f19a7eb6be6e470d28
alejo <alejo.obando.g@gmail.com>
2021-05-31 16:39:06 -0500

    search mela

--- buscar_libro.php
+++ buscar_libro.php
@@ -4,13 +4,24 @@
 
     $titulo = $_POST['titulo'];
 
-    $consulta = "SELECT * FROM libros WHERE titulo = '$titulo'";
+    $consulta = "SELECT * FROM libros WHERE codigo_libro='$titulo' OR titulo='$titulo' OR autor='$titulo' OR fecha_publicacion='$titulo' OR genero_literario='$titulo' OR numero_paginas='$titulo' OR editorial='$titulo' OR idioma='$titulo' OR estado='$titulo'";
 
     $resultados = pg_query($conn,$consulta);
 
+    $nr = pg_num_rows($resultados);
+
+    if($nr==1)
+    {
+        echo"Libro encontrado!";
+    }
+    else if ($nr == 0)
+    {
+        echo"El libro no se encuentra!";
+    }
+
     while ($resultado = pg_fetch_array($resultados)){
 
-        echo $resultado['codigo_libro']." ".$resultado['titulo']." ".$resultado['autor']." ".$resultado['fecha_publicacion']." ".$resultado['genero_literario']." ".$resultado['numero_paginas']." ".$resultado['editorial']." ".$resultado['issn']." ".$resultado['idioma']." ".$resultado['estado']." ".$resultado['precio']." ".$resultado['imagen_portada'];
+        echo $resultado['codigo_libro']." ".$resultado['titulo']." ".$resultado['autor']." ".$resultado['fecha_publicacion']." ".$resultado['genero_literario']." ".$resultado['numero_paginas']." ".$resultado['editorial']." ".$resultado['issn']." ".$resultado['idioma']." ".$resultado['estado']." ".$resultado['precio']." ".$resultado['imagen_portado'] . "<br/>";
     }
 
 ?>
--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include('conexion.php');
 
 $nombre = $_POST["txtusuario"];
 $pass = $_POST["txtpassword"];
-$query = pg_query($conn,"SELECT * FROM usuario WHERE usuario = '".$nombre."'and contraseña = '".$pass."'");
+$query = pg_query($conn,"SELECT * FROM usuario WHERE usuario = '".$nombre."'AND contraseña = '".$pass."'");
 $nr = pg_num_rows($query);
 
 if($nr==1)
