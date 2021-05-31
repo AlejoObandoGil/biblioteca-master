f251dfb2ca6681f0edfceed83d2b31e141af1c53
alejo <alejo.obando.g@gmail.com>
2021-05-30 23:20:02 -0500

    bd remote

--- conexion.php
+++ conexion.php
@@ -11,7 +11,11 @@ class Conexion{
         $dbname="biblioteca";
 
         try{
-            $conexion = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass") or die('No se ha podido conectar: ' . pg_last_error()); 
+            // bd local
+            // $conexion = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass") or die('No se ha podido conectar: ' . pg_last_error()); 
+
+            // bd remota heroku
+            $conexion = pg_connect("postgres://uqklfdmkazqmix:dd986026523d25807783d81ce26bf9743e68964dfce989fdf9db39369daa04da@ec2-54-90-211-192.compute-1.amazonaws.com:5432/dc110dq94k9s9k") or die('No se ha podido conectar: ' . pg_last_error());             
             return $conexion;
 
         }catch (Exception $e){
